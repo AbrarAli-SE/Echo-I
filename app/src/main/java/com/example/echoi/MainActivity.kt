c1b690707a524b75f7c28580cbd6265b3ccd175a
AbrarAli-SE <abrarali.se@gmail.com>
2024-11-16 22:37:05 +0500

    Creation 2 page of chating main

--- app/src/main/java/com/example/echoi/MainActivity.kt
+++ app/src/main/java/com/example/echoi/MainActivity.kt
@@ -1,26 +1,22 @@
-package com.example.echoi
-
+import android.R
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-//        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rgpp9d76y9oo))
-//        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rrhg5mlqvbm9))
-//        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rr67rjl8i2ih))
-//        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rv7brnb2s4cl))
-//        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rxh820kevmts))
-//        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rtjvhf6jjums))
-//        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rxpmz6aicpxi))
-//        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rtkuzxarfjj))
-//        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rpbfysgluja))
 
+        val inputBar = findViewById<EditText>(R.id.input_bar) // Replace with your input bar's ID
+        inputBar.setOnClickListener { v: View? ->
+            val intent = Intent(
+                this@MainActivity,
+                ChatActivity::class.java
+            )
+            startActivity(intent)
+        }
     }
 }
