1a619050d9511984da3a90d4d32acc897bc6e717
AbrarAli-SE <abrarali.se@gmail.com>
2024-11-16 20:57:02 +0500

    Project Creation

---
+++ app/src/main/java/com/example/echoi/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.echoi
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+//        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rgpp9d76y9oo))
+//        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rrhg5mlqvbm9))
+//        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rr67rjl8i2ih))
+//        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rv7brnb2s4cl))
+//        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rxh820kevmts))
+//        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rtjvhf6jjums))
+//        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rxpmz6aicpxi))
+//        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rtkuzxarfjj))
+//        Glide.with(this).load("https://i.imgur.com/1tMFzp8.png").into(findViewById(R.id.rpbfysgluja))
+
+    }
+}
