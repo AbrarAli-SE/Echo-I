9672d35f4a7126f186f605cd62c3f1d8133f36f6
AbrarAli-SE <abrarali.se@gmail.com>
2024-11-26 00:44:10 +0500

    splesh screen integrated

--- app/src/main/java/com/example/echoi/SplashActivity.kt
+++ app/src/main/java/com/example/echoi/SplashActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import android.os.Handler
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
-import android.widget.TextView
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity() {
@@ -14,13 +14,14 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        val splashText: TextView = findViewById(R.id.splash_text)
-        val animation: Animation = AnimationUtils.loadAnimation(this, R.anim.hello_animation)
-        splashText.startAnimation(animation)
+        val splashImage: ImageView = findViewById(R.id.splash_image)
+        val animation: Animation = AnimationUtils.loadAnimation(this, R.anim.fade_in_out)
+        splashImage.startAnimation(animation)
 
         Handler().postDelayed({
             startActivity(Intent(this, MainActivity::class.java))
+            overridePendingTransition(R.anim.fade_out, R.anim.fade_out)
             finish()
-        }, 3000) // Adjust the delay as needed
+        }, 3000) // Total duration should match the animation duration (1500ms fade-in + 1500ms fade-out)
     }
 }
